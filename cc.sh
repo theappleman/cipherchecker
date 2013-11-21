fd6349e9bef3566fdabb6a369bbc56fa13e0c31d
Daniel Cordero <theappleman@gmail.com>
2013-11-21 19:25:29 +0000

    Create cc.sh
    This
    is
    *the*
    Cipher
    Checker.

---
+++ cc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TOCHECK=${$1?-Needz host:port}
+
+openssl ciphers|awk 'BEGIN{RS=":"}{print$0}'|xargs -n1 openssl s_client -connect $TOCHECK -cipher 2>/dev/null|awk '/TLSv1\/SSLv3, Cipher is/{print$5}'
